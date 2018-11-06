fa6d8c36b86a914bd6f775ccc8ceced859f6f28b
Jordan Mackie <jmackie97@hotmail.com>
2018-11-06 19:55:29 +0000

    Fixed titles not showing

--- src/task1.m
+++ src/task1.m
@@ -8,23 +8,27 @@ info = imfinfo(filename);
 [r,g,b] = isolateRGB(X);
 
 figure(1)
-subplot(2,2,1);
+colorplot = subplot(2,2,1);
 imshow(X)
+title(colorplot, "Original Image (Full colour)")
 
 redplot = subplot(2,2,2);
 imshow(r)
 redcolormap = [linspace(0,1,256)', zeros(256,2)];
 colormap(redplot, redcolormap);
 colorbar
+title(redplot, "Red Components")
 
 greenplot = subplot(2,2,3);
 imshow(g)
 greencolormap = [zeros(256,1), linspace(0,1,256)', zeros(256,1)];
 colormap(greenplot, greencolormap);
 colorbar
+title(greenplot, "Green Components")
 
 blueplot = subplot(2,2,4);
 imshow(b)
 bluecolormap = [zeros(256,2), linspace(0,1,256)'];
 colormap(blueplot, bluecolormap);
 colorbar
+title(blueplot, "Blue Components")
