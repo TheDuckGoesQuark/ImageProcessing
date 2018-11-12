d639489f6bfc45b376e4adeda9907660b6d7d52f
Jordan Mackie <jmackie97@hotmail.com>
2018-11-12 20:22:52 +0000

    Got correct images for task4 report

--- src/showDCT.m
+++ src/showDCT.m
@@ -28,11 +28,13 @@ function [] = showDCT(filename, figureNumber)
 		fig2 = figure(figureNumber+2);
 		CbPlot = subplot(1,1,1);
 		imshow(log(abs(dctCr)));
+		axis square, colorbar
 		title(CbPlot, "Chromatic Blue Components")
 
 		fig3 = figure(figureNumber+3);
 		CrPlot = subplot(1,1,1);
 		imshow(log(abs(dctCb)));
+		axis square, colorbar
 		title(CrPlot, "Chromatic Red Components")
 
 		%% Extension - Show high and low frequency components
@@ -41,24 +43,23 @@ function [] = showDCT(filename, figureNumber)
 		cutoff = 0.5 * 256;
 		highFrequencies = fliplr(tril(fliplr(dcty), cutoff));
 		lowFrequencies = dcty - highFrequencies;
-				
 
 		fig4 = figure(figureNumber+4);
 		lowFreqPlot = subplot(1,1,1);
 		imshow(idct2(lowFrequencies));
-		axis square, colorbar
 		title(lowFreqPlot, "Low Frequency Components")
 
 		fig5 = figure(figureNumber+5);
 		highFreqPlot = subplot(1,1,1);
 		imshow(idct2(highFrequencies));
-		axis square, colorbar
 		title(highFreqPlot, "High Frequency Component")
 
 		% Save figures to file
-	%	saveas(fig1, strcat(filename, "y.png"));
-	%	saveas(fig2, strcat(filename, "cb.png"));
-	%	saveas(fig3, strcat(filename, "cr.png"));
-	%	saveas(fig4, strcat(filename, "hf.png"));
-	%	saveas(fig5, strcat(filename, "lf.png"));
+		[~, filename, ~] = fileparts(filename);
+		dir = "images/task4/";
+		saveas(fig1, strcat(dir, filename, "y.png"));
+		saveas(fig2, strcat(dir, filename, "cb.png"));
+		saveas(fig3, strcat(dir, filename, "cr.png"));
+		saveas(fig4, strcat(dir, filename, "hf.png"));
+		saveas(fig5, strcat(dir, filename, "lf.png"));
 end
